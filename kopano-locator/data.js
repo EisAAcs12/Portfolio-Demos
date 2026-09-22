92e5c487fed5e46713c28e06d61fedb6fcccc2ce
Enziano Isaacs <enziano@amathuba-hub.co.za>
2026-09-22 19:31:55 +0200

    Update SHEET_CSV_URL for Kopano locator

--- kopano-locator/data.js
+++ kopano-locator/data.js
@@ -23,7 +23,7 @@ const BRANDS = {
     nameAccent: "Media",
     tagline: "OOH Site Locator",
     CONFIG: {
-      SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4EqCasg5kocxv3WLRROsH8nCxJnIsEu4PuV2GkLeZQyfqYrWULYYquSaSx-YPaV8rEa3YC2o\_kzoE/pub?gid=1481682342\&single=true\&output=csv",
+      SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4EqCasg5kocxv3WLRROsH8nCxJnIsEu4PuV2GkLeZQyfqYrWULYYquSaSx-YPaV8rEa3YC2o_kzoE/pub?gid=1481682342&single=true&output=csv",
       REFRESH_SECONDS: 45,
     },
     CONTACT: {
@@ -327,7 +327,7 @@ const BRANDS = {
     nameAccent: "Medium",
     tagline: "OOH Site Locator",
     CONFIG: {
-      SHEET_CSV_URL: "",
+      SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxNYqjitaphJreeyzjyENLZ97R56ykWH_3zxkvsLtTaVtCUKFiUHQhgsjcVzAv3Fhdv9DcM2QwHFch/pub?gid=1662957922&single=true&output=csv",
       REFRESH_SECONDS: 45,
     },
     CONTACT: {
